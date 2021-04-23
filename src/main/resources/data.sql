5ed1159819ab9a0b0fd3f0bce6141787ca33c53b
Rakhee99 <80023884+Rakhee99@users.noreply.github.com>
2021-04-23 14:16:44 +0530

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 insert into content values (101 ,'somethng isnt working','bug');
 insert into content values (102,'Improvements or additions to documentation', 'documention');
-insert into content values (103,'This issue and pull request already exists', 'duplicate');
+insert into content values (103,'This issue or pull request already exists', 'duplicate');
 insert into content values (104,'New feature or request', 'enhacement');
 insert into content values (105,'This doesnt seem right', 'invalid');
